2fe38854d4f764d6a9bee9b0ce8dc83c4c696c8f
dzmitrytrystsen <tristendmitry@gmail.com>
2018-10-04 19:39:02 +0300

    Ball stack to paddle ready

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    [SerializeField] public PaddleScript paddle;
+    public Vector2 paddleToBallVector;
+
+    void Start ()
+    {
+        paddleToBallVector = transform.position;
+    }
+	
+	void Update ()
+	{
+        Vector2 paddlePos = new Vector2(paddle.transform.position.x, transform.position.y);
+	    transform.position = paddlePos;
+	}
+}
