9b6e43a60ce0febbf9487f281bcf061e7a3e8a97
dzmitrytrystsen <tristendmitry@gmail.com>
2018-10-05 13:05:47 +0300

    Start ready

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 public class BallScript : MonoBehaviour
 {
     [SerializeField] public PaddleScript paddle;
+    [SerializeField] public float bounceBallX = -2f;
+    [SerializeField] public float bounceBallY = 15f;
+
     public Vector2 paddleToBallVector;
+    public bool hasStarted = false;
+
 
     void Start ()
     {
@@ -13,8 +18,26 @@ public class BallScript : MonoBehaviour
     }
 	
 	void Update ()
-	{
+    {
+        if (!hasStarted)
+        {
+            BallToPaddle();
+            BounceTheBall();
+        }
+    }
+
+    public void BallToPaddle()
+    {
         Vector2 paddlePos = new Vector2(paddle.transform.position.x, transform.position.y);
-	    transform.position = paddlePos;
-	}
+        transform.position = paddlePos;
+    }
+
+    public void BounceTheBall()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(bounceBallX, bounceBallY);
+            hasStarted = true;
+        }
+    }
 }
