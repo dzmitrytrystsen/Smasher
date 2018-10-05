646fb4832e579aeef36dce1f087059c02078c08d
dzmitrytrystsen <tristendmitry@gmail.com>
2018-10-05 18:21:34 +0300

    +

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -7,14 +7,18 @@ public class BallScript : MonoBehaviour
     [SerializeField] public PaddleScript paddle;
     [SerializeField] public float bounceBallX = -2f;
     [SerializeField] public float bounceBallY = 15f;
+    [SerializeField] public AudioClip[] fireballSounds;
 
     public Vector2 paddleToBallVector;
     public bool hasStarted = false;
 
+    // Cashed components references
+    public AudioSource myAudioSource;
 
     void Start ()
     {
         paddleToBallVector = transform.position;
+        myAudioSource = GetComponent<AudioSource>();
     }
 	
 	void Update ()
@@ -38,6 +42,15 @@ public class BallScript : MonoBehaviour
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(bounceBallX, bounceBallY);
             hasStarted = true;
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (hasStarted)
+        {
+            AudioClip clips = fireballSounds[Random.Range(0, fireballSounds.Length)];
+            myAudioSource.PlayOneShot(clips);
         }
     }
 }
---
+++ Assets/Scripts/BrickScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickScript : MonoBehaviour
+{
+    [SerializeField] public AudioClip crackSound;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        AudioSource.PlayClipAtPoint(crackSound, Camera.main.transform.position);
+        Destroy(gameObject);
+    }
+}
