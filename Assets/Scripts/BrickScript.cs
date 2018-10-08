30d223f1d1f2bc0360b95083350d12b19c34e29d
dzmitrytrystsen <tristendmitry@gmail.com>
2018-10-08 16:35:14 +0300

    VFX added

--- Assets/Scripts/BrickScript.cs
+++ Assets/Scripts/BrickScript.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Object = System.Object;
 
 public class BrickScript : MonoBehaviour
 {
     [SerializeField] public AudioClip crackSound;
+    [SerializeField] public GameObject explosionVFX;
 
     // Cached reference
     public LevelScript level;
@@ -17,8 +19,21 @@ public class BrickScript : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        if (tag == "Breakable")
+            DestroyBlock();
+    }
+
+    public void TriggerExplosion()
+    {
+        GameObject explosion = Instantiate(explosionVFX, transform.position, transform.rotation);
+        Destroy(explosion, 1f);
+    }
+
+    public void DestroyBlock()
+    {
         AudioSource.PlayClipAtPoint(crackSound, Camera.main.transform.position);
         Destroy(gameObject);
         level.SubtractBreakableBricks();
+        TriggerExplosion();
     }
 }
--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -1,16 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
-public class GameStatus : MonoBehaviour {
+public class GameStatus : MonoBehaviour
+{
+    [Range(1f, 3f)] [SerializeField] public float gameSpeed = 1f;
 
-	// Use this for initialization
-	void Start () {
+    //How to save data from the prev scene
+    //But don't forget to child all text you need under GameStatus object
+    //private void Awake()
+    //{
+    //    int gameStatusCount = FindObjectsOfType<GameStatus>().Length;
+    //    if (gameStatusCount > 1) 
+    //        Destroy(gameObject);
+    //    else 
+    //        DontDestroyOnLoad(gameObject);
+    //}
+
+	void Start ()
+	{
 		
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+	{
+	    Time.timeScale = gameSpeed;
 	}
 }
